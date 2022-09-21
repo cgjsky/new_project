681af2f688638d3a2baeb2248d5e068e2f6a7329
cgj <cgj1104862655@126.com>
2022-09-21 23:24:28 +0800

    learn fastapi

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-a = ["A", "B", "C"]
-b = ["B", "A"]
-c = all(map(lambda x: x in a, b))
-print(c)
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 1):
+    return fake_items_db[skip: skip + limit]
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app)
